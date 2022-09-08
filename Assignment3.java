d69d0fbd1b7e1ed8f82c139e08811b3116a7ac94
Suvan Kumar <105762554+suvan-kumar@users.noreply.github.com>
2022-09-07 21:44:42 -0700

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,56 @@
+Assignment #: 3
+Name: Suvan Kumar
+StudentID: 1221910898
+Lecture: MWF 10:10 - 11am
+
+public class Assignment3 {
+	
+1. PUBLIC – Software engineers shall act consistently with the public interest.
+
+-- Every decision a software engineer makes, from releasing software to working on
+projects, has to consider the welfare of the public. Nothing that puts people in harms way 
+should be produced. This includes taking the blame and responsibility for software that is 
+produced, revealing any possible issues with the software, and including accessibility for
+everyone within the software. An example of this is not releasing a social media platform
+that affects the mental health of users. 
+
+2. CLIENT AND EMPLOYER – Software engineers shall act in a manner that is in the best 
+interests of their client and employer consistent with the public interest.
+
+-- Software engineers are expected to make decisions that are asked for and benefit the client
+and employer. This means that one shouldn't abuse their power and accessibility to exploit 
+the information or property that they have access to. Side projects or other jobs should not 
+be taken unless it is absolutely guaranteed that it would not be detrimental to the work of the
+job. Any choice that has to be made by a software engineer should be to help the client and employer,
+unequivocally. An example of this is to maintain the privacy of an employer and not selling trade secrets to
+competitors. 
+
+3. JUDGMENT – Software engineers shall maintain integrity and independence in their professional 
+judgment.
+
+-- The integrity and independence of a software engineer must be unwavering. Bribes and
+conflicts of interest should be avoided at all costs. Evaluations of sotware should be strictly
+objective and not take character of an individual or opinions into consideration. An example of this is
+to turn down a bribe from a competitor to tank the release of software.
+
+4. MANAGEMENT – Software engineering managers and leaders shall subscribe to and promote an ethical 
+approach to the management of software development and maintenance.
+
+-- Management in software engineering have a large amount of power and influence over the software
+engineers in their team. This is not something that should be taken lightly. The engineers in a team
+are responsible for looking over the engineers and ensuring that their work is meeting the standards
+that are set forth. Hearing out engineers and giving out assignments that are fair and meeting an
+engineer's abilities is expected as well. An example of this is giving an employee guidance when they
+have concerns on how they should go about implementing a solution without affecting the integrity of the 
+software.
+
+5. COLLEAGUES – Software engineers shall be fair to and supportive of their colleagues.
+
+-- Engineers work in teams and should be held responsible for ensuring that their colleagues
+maintain integrity and follow the Software Engineering Code to the best of their abilites. 
+Assisting colleagues in their work and making sure that the best and safest product is shipped
+to clients is part of the job description. Using colleagues for assistance, especially in an area
+that is their expertise, is highly recommended. An example of this is giving credit where credit is due,
+especially if it is the work of a colleague. 
+
+}
